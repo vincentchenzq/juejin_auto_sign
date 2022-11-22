8aec9785451cdca1e91a3416347a627f2acc85de
chenzhiq <chenzhiq@authine.com>
2022-11-22 20:51:30 +0800

    fix: env 大写

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const got = require('got')
 
 const { cookie, aid, uuid, _signature, PUSH_PLUS_TOKEN } = require('./config')
+console.log(cookie, aid, uuid, _signature, PUSH_PLUS_TOKEN)
 
 const BASEURL = 'https://api.juejin.cn/growth_api/v1/check_in' // 掘金签到api
 const PUSH_URL = 'http://www.pushplus.plus/send' // pushplus 推送api
